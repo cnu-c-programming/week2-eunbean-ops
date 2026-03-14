b6e0124c35ce15eba170f2be1e64256c382af0c8
eunbean-ops <rida1223@naver.com>
2026-03-14 16:47:19 +0900

    3-4
    Refactor main function to include variable assignments and calculations.

--- code/3-4.c
+++ code/3-4.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
-
-int main()
-{
+int main(){
+    int a,b,c;
+    a=b=c=5;
+    printf("%d %d %d\n", a,b,c);
+    a=2+3*4;
+    printf("%d %d %d\n", a,b,c);
+    // a= 14
+    c= a++ + ++b;
+    //c= 14+ 6= 20,  a는 15됨
+    // a=15, b=6, c= 20
+    printf("%d %d %d\n", a,b,c);
 
     return 0;
 }
-
